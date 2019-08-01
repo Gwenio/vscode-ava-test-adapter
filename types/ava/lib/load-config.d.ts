de09e96acf1031d599bec8c3312f1f45ef926034
James Adam Armstrong <gwenio@live.com>
2019-08-01 14:49:35 -0500

    Added load-config types for AVA.

---
+++ types/ava/lib/load-config.d.ts
@@ -0,0 +1,55 @@
+/*
+ISC License (ISC)
+
+Copyright 2019 James Adam Armstrong
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted, provided that the above copyright
+notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+PERFORMANCE OF THIS SOFTWARE.
+*/
+
+/* eslint-disable */ // VSCode ESLint plugin does not respect .eslintignore
+
+declare module 'ava/lib/load-config'
+
+namespace AVA {
+	interface Configuration {
+		files?: string[];
+		helpers?: string[];
+		sources?: string[];
+		match?: string[];
+		cache?: boolean;
+		failFast?: boolean;
+		failWithoutAssertions?: boolean;
+		environmentVariables?: { [key: string]: string };
+		tap?: boolean;
+		verbose?: boolean;
+		snapshotDir?: string;
+		compileEnhancements?: boolean;
+		extensions?: string[];
+		require?: string[];
+		babel?: {
+			extensions?: string[];
+		};
+		timeout?: number;
+	}
+
+	interface Parameters {
+		configFile?: string;
+		resolveFrom: string;
+		defaults: Configuration;
+	}
+}
+
+export default function loadConfig({
+	configFile,
+	resolveFrom = process.cwd(),
+	defaults = {} }: AVA.Parameters = {}): AVA.Configuration
