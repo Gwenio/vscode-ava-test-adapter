d6e67deaddddc1f08d94ee6e45993ab229cd4305
James Adam Armstrong <gwenio@live.com>
2019-08-05 11:28:48 -0500

    Added abstract Reporter class.

---
+++ src/worker/reporter.ts
@@ -0,0 +1,41 @@
+/*
+ISC License (ISC)
+
+Copyright 2019 James Adam Armstrong
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted, provided that the above copyright
+notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+PERFORMANCE OF THIS SOFTWARE.
+*/
+
+import { AVA } from '../../types/ava/namespace'
+
+type RemoveListener = () => void
+
+export default abstract class Reporter implements AVA.Reporter {
+	private removeListener: null | RemoveListener = null
+
+	public reset(): void {
+		if (this.removeListener) {
+			this.removeListener()
+			this.removeListener = null
+		}
+	}
+
+	public startRun(plan: AVA.Plan): void {
+		this.reset()
+		this.removeListener = plan.status.on('stateChange',
+			(event): void => { this.consumeStateChange(event) })
+	}
+
+	public abstract consumeStateChange(event: AVA.Event): void
+	public abstract endRun(): void
+}
