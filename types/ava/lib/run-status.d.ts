6191dc58ed128cbbed8794ae43db145e892e19bb
James Adam Armstrong <gwenio@live.com>
2019-08-26 10:49:05 -0500

    Added type definition for ava run-status.

---
+++ types/ava/lib/run-status.d.ts
@@ -0,0 +1,30 @@
+/*
+ISC License (ISC)
+
+Copyright 2019 James Adam Armstrong
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted, provided that the above copyright
+notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+PERFORMANCE OF THIS SOFTWARE.
+*/
+
+/* eslint-disable */ // VSCode ESLint plugin does not respect .eslintignore
+
+import AVA from "../namespace"
+
+declare module 'ava/lib/run-status' {
+	export default class Status implements AVA.Status {
+		constructor(files: string[], parallelRuns: null | number);
+		on(tag: string, handler: (event: Event) => void): () => void;
+		suggestExitCode(circumstances: { matching: boolean }): number;
+		emitStateChange(event: Event): void;
+	}
+}
