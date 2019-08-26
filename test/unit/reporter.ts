50d2cedfe807bf4c6d261c45d0501725d2c7e6b0
James Adam Armstrong <gwenio@live.com>
2019-08-26 11:59:59 -0500

    Basic reporter tests.

---
+++ test/unit/reporter.ts
@@ -0,0 +1,99 @@
+/*
+ISC License (ISC)
+
+Copyright 2019 James Adam Armstrong
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted, provided that the above copyright
+notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+PERFORMANCE OF THIS SOFTWARE.
+*/
+
+import Emitter from 'events'
+import anyTest, { TestInterface } from 'ava'
+import sinon, { SinonSandbox } from 'sinon'
+import Status from 'ava/lib/run-status'
+import {
+	LoadReporter,
+	TestReporter,
+	DebugReporter
+} from '../../src/reporter'
+
+interface Context {
+	/**
+	 * @summary Isolated sinon sandbox.
+	 */
+	sandbox: SinonSandbox;
+}
+
+const test = anyTest as TestInterface<Context>
+
+// Setup isolated sandbox
+test.beforeEach(async (t): Promise<void> => {
+	t.context.sandbox = sinon.createSandbox()
+})
+
+const basePlan = {
+	clearLogOnNextRun: false,
+	failFastEnabled: false,
+	filePathPrefix: '',
+	files: [],
+	matching: true,
+	previousFailures: 0,
+	runOnlyExclusive: true,
+	runVector: 0
+}
+
+test('LoadReporter begin and end log', async (t): Promise<void> => {
+	const l = t.context.sandbox.spy()
+	const status = new Status([], null)
+	const r = new LoadReporter([], l)
+	r.startRun({
+		...basePlan,
+		status
+	})
+	r.endRun()
+	r.endRun()
+	t.is(l.callCount, 2)
+})
+
+test('TestReporter begin and end log', async (t): Promise<void> => {
+	const l = t.context.sandbox.spy()
+	const emit = new Emitter()
+	const status = new Status([], null)
+	const r = new TestReporter(emit, 0, l)
+	r.startRun({
+		...basePlan,
+		status
+	})
+	r.endRun()
+	r.endRun()
+	t.is(l.callCount, 2)
+})
+
+test('DebugReporter begin and end log', async (t): Promise<void> => {
+	const l = t.context.sandbox.spy()
+	const s1 = new Status([], null)
+	const s2 = new Status([], null)
+	const r = new DebugReporter((_p: number): void => { }, 9229, l)
+	r.startRun({
+		...basePlan,
+		status: s1
+	})
+	t.throws((): void => {
+		r.startRun({
+			...basePlan,
+			status: s2
+		})
+	})
+	r.endRun()
+	r.endRun()
+	t.is(l.callCount, 2)
+})
