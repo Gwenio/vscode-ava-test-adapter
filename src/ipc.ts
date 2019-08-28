09ca83e720264ef99e7b7f6a2b54ab0bdf0dd3d1
James Adam Armstrong <gwenio@live.com>
2019-08-28 12:56:19 -0500

    Documented ipc.

--- src/ipc.ts
+++ src/ipc.ts
@@ -16,78 +16,116 @@ OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 PERFORMANCE OF THIS SOFTWARE.
 */
 
+/** The base type for IPC messages. */
 interface Base<T extends string> {
+	/** The message type. */
 	type: T
 }
 
+/** Message to control the worker's logging. */
 export interface Logging extends Base<'log'> {
+	/** Whether to enable or disable logging. */
 	enable: boolean
 }
 
+/** Message to load a configuration file. */
 export interface Load extends Base<'load'> {
+	/** The configuration file name relative to the worker's CWD. */
 	file: string
 }
 
+/** Message to drop a loaded configuration. */
 export interface Drop extends Base<'drop'> {
+	/** The ID of the configuration to drop or undefined to drop all. */
 	id?: string
 }
 
+/** Message with information on a newly loaded configuration. */
 export interface Prefix extends Base<'prefix'> {
+	/** The ID assigned to the configuration. */
 	id: string
+	/** The configuration file name relative to the worker's CWD. */
 	file: string
+	/** The common path prefix of the configuration's test files. */
 	prefix: string
 }
 
+/** Message with information on a test file. */
 export interface TestFile extends Base<'file'> {
+	/** The ID assigned to the test file. */
 	id: string
+	/** The ID of the configuration the test file belongs to. */
 	config: string
+	/** The test file containing the test case. */
 	file: string
 }
 
+/** Message with information on a test case. */
 export interface TestCase extends Base<'case'> {
+	/** The ID assigned to the configuration. */
 	id: string
+	/** The test file name relative to the common prefix. */
 	file: string
+	/** The test's title. */
 	test: string
 }
 
+/** Message that triggers a test run. */
 export interface Run extends Base<'run'> {
+	/** Array of IDs to include in the run. */
 	run: string[]
 }
 
+/** Message to stop an active run. */
 export type Stop = Base<'stop'>
 
+/** Message containing the result of a test. */
 export interface Result extends Base<'result'> {
+	/** The ID of the test. */
 	test: string
+	/** The test state for the adapter. */
 	state: 'running' | 'passed' | 'failed' | 'skipped' | 'errored'
 }
 
+/** Message signalling that a configuration or test file is complete. */
 export interface Done extends Base<'done'> {
+	/** The configuration or test file ID. */
 	file: string
 }
 
+/** Message triggering a debugging run of tests. */
 export interface Debug extends Base<'debug'> {
+	/** The preferred inspect port. */
 	port: number
+	/** Contains information of which configurations to force to be serial. */
 	serial: {
-		/**
-		 * @summary A configuration is to be debugged serially if x !== list.includes(id).
-		 */
+		/** A configuration is to be debugged serially if x !== list.includes(id). */
 		x: boolean
+		/** Array of configuration IDs. */
 		list: string[]
 	}
+	/** Array of IDs to include in the run. */
 	run: string[]
 }
 
 export interface Ready extends Base<'ready'> {
+	/** The configuration being debugged. */
 	config: string
+	/** The inspect port to use. */
 	port: number
 }
 
+/** Messages that trigger actions in the worker. */
 export type Action = Load | Drop | Run | Stop | Debug
 
+/** Messages sent by the parent. */
 export type Parent = Action | Logging
 
+/** Messages for building the TestTree. */
 export type Tree = Prefix | TestFile | TestCase
 
+/** Test run event messages. */
 export type Event = Result | Done
 
+/** Messages sent by the child. */
 export type Child = Tree | Event | Ready
