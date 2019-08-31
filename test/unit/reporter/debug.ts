9295223296ae0f1caa40bbe467d4055197075aea
James Adam Armstrong <gwenio@live.com>
2019-08-30 19:45:06 -0500

    Test debug reporter port selection.

--- test/unit/reporter/debug.ts
+++ test/unit/reporter/debug.ts
@@ -50,9 +50,10 @@ const basePlan = {
 
 test('begin and end log', async (t): Promise<void> => {
 	const l = t.context.sandbox.spy()
+	const ready = t.context.sandbox.spy()
 	const s1 = new Status([], null)
 	const s2 = new Status([], null)
-	const r = new DebugReporter((_p: number): void => {}, 9229, l)
+	const r = new DebugReporter(ready, 9229, l)
 	r.startRun({
 		...basePlan,
 		status: s1,
@@ -66,4 +67,21 @@ test('begin and end log', async (t): Promise<void> => {
 	r.endRun()
 	r.endRun()
 	t.is(l.callCount, 2)
+	t.is(ready.callCount, 1)
+})
+
+test('select inspect port', async (t): Promise<void> => {
+	const ready = t.context.sandbox.spy()
+	const status = new Status([], null)
+	const r = new DebugReporter(ready, 9229)
+	t.notThrows((): void => {
+		const port = r.selectPort()
+		r.startRun({
+			...basePlan,
+			status,
+		})
+		r.endRun()
+		t.is(ready.callCount, 1)
+		t.true(ready.calledWith(port))
+	})
 })
