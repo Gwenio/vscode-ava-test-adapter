8853268d470ddded84ae38ff4a500c14bc0f0e64
James Adam Armstrong <gwenio@live.com>
2019-09-20 21:50:54 -0500

    refactor(config): rewrite

--- src/adapter/config.ts
+++ src/adapter/config.ts
@@ -17,123 +17,30 @@ PERFORMANCE OF THIS SOFTWARE.
 */
 
 import path from 'path'
-import vscode from 'vscode'
-import { detectNodePath } from 'vscode-test-adapter-util/out/misc'
+import ow from 'ow'
 import Log from './log'
 
 /** The root for the extension's VSCode configurations. */
 export const configRoot = 'avaExplorer'
 
-/** Contains utilities for managing configuration. */
-export class AVAConfig {
-	/**
-	 * Checks if a configuration change affects certain options.
-	 * @param uri The URI for the workspace.
-	 * @param event The ConfigurationChangeEvent.
-	 * @param settings The settings to check if they are affected.
-	 * @returns Whether or not the set of options were affected.
-	 */
-	public static affected(
-		uri: vscode.Uri,
-		event: vscode.ConfigurationChangeEvent,
-		...settings: string[]
-	): boolean {
-		const base = `${configRoot}.`
-		for (const check of settings) {
-			if (event.affectsConfiguration(`${base}${check}`, uri)) {
-				return true
-			}
-		}
-		return false
-	}
-
-	/**
-	 * Loads the configuration.
-	 * @param uri The URI for the workspace.
-	 * @param log The Log to output to.
-	 */
-	public static async load(uri: vscode.Uri, log: Log): Promise<LoadedConfig | null> {
-		const adapterConfig = vscode.workspace.getConfiguration(configRoot, uri)
-
-		const cwd = path.resolve(uri.fsPath, adapterConfig.get<string>('cwd') || '')
-
-		const configs: SubConfig[] = adapterConfig.get<SubConfig[]>('configs') || [
-			{
-				file: 'ava.config.js',
-				serial: false,
-				debuggerSkipFiles: [],
-			},
-		]
-
-		let nodePath: string | undefined = adapterConfig.get<string>('nodePath') || undefined
-		if (nodePath === 'default' || nodePath === undefined) {
-			nodePath = await detectNodePath()
-		}
-
-		const configEnvironment: NodeJS.ProcessEnv = {
-			...(adapterConfig.get('env') || {}),
-		}
-		if (log.enabled) {
-			const output = JSON.stringify(configEnvironment)
-			log.debug(`Using environment variable config: ${output}`)
-		}
-		const environment: NodeJS.ProcessEnv = { ...process.env }
-		for (const key in configEnvironment) {
-			const value = configEnvironment[key]
-			if (value === undefined || value === null) {
-				delete environment.key
-			} else {
-				environment[key] = String(value)
-			}
-		}
-
-		if (log.enabled) log.debug(`Using nodePath: ${nodePath}`)
-
-		const nodeArgv: string[] = adapterConfig.get<string[]>('nodeArgv') || []
-		if (log.enabled) log.debug(`Using node arguments: ${nodeArgv}`)
-
-		const debuggerPort = adapterConfig.get<number>('debuggerPort') || 9229
-
-		const debuggerSkipFiles = adapterConfig.get<string[]>('debuggerSkipFiles') || []
-
-		return {
-			cwd,
-			configs: configs.map(
-				(c): SubConfig => {
-					return {
-						file: c.file || 'ava.config.js',
-						serial: c.serial || false,
-						debuggerSkipFiles: c.debuggerSkipFiles,
-					}
-				}
-			),
-			environment,
-			nodePath,
-			nodeArgv,
-			debuggerPort,
-			debuggerSkipFiles,
-		}
-	}
-}
-
 /** Stores configuration for as specific AVA configuration. */
 export interface SubConfig {
 	/** The AVA configuration file name. */
-	file: string
+	readonly file: string
 	/** Whether to run tests serially while debugging. */
-	serial: boolean
+	readonly serial: boolean
 	/** Additional files to skip while debugging. */
-	debuggerSkipFiles: string[]
+	readonly debuggerSkipFiles: string[]
 }
 
-/** The loaded configuration. */
-export interface LoadedConfig {
+/** A cached set of configuration values. */
+interface ConfigCache {
 	/** The current working directory for the worker. */
 	cwd: string
 	/** The sub-configurations. */
 	configs: SubConfig[]
 	/** The environment for the Worker. */
-	environment: NodeJS.ProcessEnv
+	environment: Readonly<NodeJS.ProcessEnv>
 	/** The path to the NodeJS executable to use. */
 	nodePath: string | undefined
 	/** The CLI arguments for Node. */
@@ -142,4 +49,179 @@ export interface LoadedConfig {
 	debuggerPort: number
 	/** Files to skip while debugging. */
 	debuggerSkipFiles: string[]
+}
+
+/** A set of configuration values. */
+export type LoadedConfig = Readonly<ConfigCache>
+
+const subDefault: Readonly<SubConfig> = {
+	file: 'ava.config.js',
+	serial: false,
+	debuggerSkipFiles: [],
+}
+
+const configDefaults: LoadedConfig = {
+	cwd: '',
+	configs: [subDefault],
+	environment: {},
+	nodePath: undefined,
+	nodeArgv: [],
+	debuggerPort: 9229,
+	debuggerSkipFiles: [],
+}
+
+const configValidate: { [K in keyof LoadedConfig]: (_: LoadedConfig[K]) => void } = {
+	cwd: ow.create('cwd', ow.string),
+	configs: ow.create(
+		'configs',
+		ow.array.nonEmpty.ofType(
+			ow.object.partialShape({
+				file: ow.any(ow.nullOrUndefined, ow.string.nonEmpty),
+				serial: ow.optional.boolean,
+				debuggerSkipFiles: ow.optional.array.ofType(ow.string.nonEmpty),
+			})
+		)
+	),
+	environment: ow.create('env', ow.object.plain.valuesOfType(ow.string)),
+	nodePath: ow.create('nodePath', ow.any(ow.undefined, ow.string.nonEmpty)),
+	nodeArgv: ow.create('nodeArgv', ow.array.ofType(ow.string.nonEmpty)),
+	debuggerPort: ow.create('debuggerPort', ow.number.lessThanOrEqual(65535)),
+	debuggerSkipFiles: ow.create('debuggerSkipFiles', ow.optional.array.ofType(ow.string.nonEmpty)),
+}
+
+export type ConfigKey = keyof LoadedConfig
+
+const configAliases = {
+	environment: 'env',
+}
+
+interface ConfigMap {
+	get(key: ConfigKey): string
+	keys(): IterableIterator<ConfigKey>
+	entries(): IterableIterator<[ConfigKey, string]>
+}
+
+const configAliasMap = new Map<ConfigKey, string>(
+	Object.keys(configValidate).map((key): [ConfigKey, string] => [
+		key as ConfigKey,
+		configAliases[key] || key,
+	])
+) as ConfigMap
+
+const configMap = new Map<ConfigKey, string>(
+	Object.keys(configValidate).map((key): [ConfigKey, string] => [
+		key as ConfigKey,
+		`${configRoot}.${configAliases[key] || key}`,
+	])
+) as ConfigMap
+
+export interface ConfigQuery {
+	<T>(key: string): T | undefined
+}
+
+export type ConfigUpdate = (key: string) => boolean
+
+/** Contains utilities for managing configuration. */
+export class Config<X extends string> {
+	private readonly defaultNode?: string
+	private readonly fsPath: string
+	private readonly extra = new Map<X, string>()
+	private readonly log: Log
+	private cache: LoadedConfig
+
+	public get current(): LoadedConfig {
+		return this.cache
+	}
+
+	public constructor(
+		fsPath: string,
+		query: ConfigQuery,
+		log: Log,
+		additional: X[],
+		node?: string
+	) {
+		this.fsPath = fsPath
+		this.defaultNode = node
+		this.log = log
+		const initial: ConfigCache = { ...configDefaults }
+		for (const k of configMap.keys()) {
+			this.setValue(initial, k, query)
+		}
+		this.cache = initial
+		for (const k of additional) {
+			this.extra.set(k, `${configRoot}.${k}`)
+		}
+	}
+
+	public update(event: ConfigUpdate, query: ConfigQuery): Set<ConfigKey | X> {
+		const affected = new Set<ConfigKey | X>()
+		const c: ConfigCache = { ...this.cache }
+		for (const [key, alias] of configMap.entries()) {
+			if (event(alias)) {
+				this.log.info(`${key} updated`)
+				affected.add(key)
+				this.setValue(c, key, query)
+			}
+		}
+		if (affected.size > 0) {
+			this.cache = c
+		}
+		for (const [key, alias] of this.extra.entries()) {
+			if (event(alias)) {
+				affected.add(key)
+			}
+		}
+		return affected
+	}
+
+	private getValue<K extends ConfigKey>(key: K, query: ConfigQuery): LoadedConfig[K] {
+		const value = query<LoadedConfig[K]>(configAliasMap.get(key))
+		if (value === undefined) {
+			this.log.info(`No value for ${key} set, using default.`)
+			return configDefaults[key]
+		} else {
+			try {
+				const f = configValidate[key] as (_: LoadedConfig[K]) => void
+				f(value)
+			} catch (error) {
+				this.log.error(`Found invalid value for ${configMap.get(key)}`, error)
+				return configDefaults[key]
+			}
+			return value
+		}
+	}
+
+	private setValue(c: ConfigCache, key: ConfigKey, query: ConfigQuery): void {
+		switch (key) {
+			case 'cwd':
+				c[key] = path.resolve(this.fsPath, this.getValue(key, query))
+				return
+			case 'configs':
+				c[key] = this.getValue(key, query).map(
+					({ file, serial, debuggerSkipFiles }): SubConfig => {
+						return {
+							file: file || subDefault.file,
+							serial: serial === true,
+							debuggerSkipFiles: debuggerSkipFiles || [],
+						}
+					}
+				)
+				return
+			case 'nodePath':
+				c[key] = this.getValue(key, query) || this.defaultNode
+				return
+			case 'environment':
+				c[key] = this.getValue(key, query)
+				return
+			case 'nodeArgv':
+				c[key] = this.getValue(key, query)
+				return
+			case 'debuggerPort':
+				c[key] = this.getValue(key, query)
+				return
+			case 'debuggerSkipFiles':
+				c[key] = this.getValue(key, query)
+				return
+		}
+	}
 }
