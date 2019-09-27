17f2ca8b5e833b8882920eff307480b4eb6d0fd1
James Adam Armstrong <gwenio@live.com>
2019-09-27 10:54:22 -0500

    build(pnpm): limit package aliasing

--- pnpmfile.js
+++ pnpmfile.js
@@ -6,21 +6,26 @@ module.exports = {
 	},
 }
 
+const remap = new Set(['random'])
+
 function readPackage(pack) {
-	const aliases = {
-		'ow-lite': 'npm:ow@^0.13.2',
-	}
-	for (const d of [
-		'dependencies',
-		'devDependencies',
-		'peerDependencies',
-		'optionalDependencies',
-	]) {
-		const dependencies = pack[d]
-		if (dependencies) {
-			for (const [key, value] of Object.entries(aliases)) {
-				if (dependencies[key]) {
-					dependencies[key] = value
+	if (remap.has(pack.name)) {
+		const types = [
+			'dependencies',
+			'devDependencies',
+			'peerDependencies',
+			'optionalDependencies',
+		]
+		const aliases = {
+			'ow-lite': 'npm:ow@^0.13.2',
+		}
+		for (const d of types) {
+			const dependencies = pack[d]
+			if (dependencies) {
+				for (const [key, value] of Object.entries(aliases)) {
+					if (dependencies[key]) {
+						dependencies[key] = value
+					}
 				}
 			}
 		}
